e543d78f9c26deff03768d5fbcd5c527df54f02f
User <User@Mayu>
2016-05-25 15:38:22 -0500

    avance 25

---
+++ Documents/NetBeansProjects/Operaciones1/src/operaciones1/Operaciones1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operaciones1;
+
+/**
+ *
+ * @author Paos
+ */
+public class Operaciones1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Operaciones1 n=new Operaciones1();
+        n.setLocationRelativeTo(null);
+        n.setVisible(true);
+        
+        
+    }
+
+    private void setLocationRelativeTo(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
